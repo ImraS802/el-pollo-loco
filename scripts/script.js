213f711fc1b7ae8a1dc0af9d5afef779a22e0f71
imra.s <i.skaliks.is@gmail.com>
2025-09-19 14:02:57 +0200

    add keyboard functions

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,51 @@
 let canvas;
 let world;
+let keyboard = new Keyboard();
 
 function init() {
   canvas = document.getElementById('canvas');
-  world = new World(canvas);
+  world = new World(canvas, keyboard);
 
   console.log('My character is', world.character);
 }
+
+// move with arrow keys on keyboard
+window.addEventListener('keydown', (event) => {
+  switch (event.key) {
+    case 'ArrowLeft':
+      keyboard.LEFT = true;
+      break;
+    case 'ArrowRight':
+      keyboard.RIGHT = true;
+      break;
+    case 'ArrowUp':
+      keyboard.UP = true;
+      break;
+    case 'ArrowDown':
+      keyboard.DOWN = true;
+      break;
+    case ' ':
+      keyboard.SPACE = true;
+      break;
+  }
+});
+
+window.addEventListener('keyup', (event) => {
+  switch (event.key) {
+    case 'ArrowLeft':
+      keyboard.LEFT = false;
+      break;
+    case 'ArrowRight':
+      keyboard.RIGHT = false;
+      break;
+    case 'ArrowUp':
+      keyboard.UP = false;
+      break;
+    case 'ArrowDown':
+      keyboard.DOWN = false;
+      break;
+    case ' ':
+      keyboard.SPACE = false;
+      break;
+  }
+});
