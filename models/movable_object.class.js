00cac0e52adfed6f754e97d6f3972c898ccee9b9
imra.s <i.skaliks.is@gmail.com>
2025-09-21 18:18:09 +0200

    add space key jump

--- models/movable_object.class.js
+++ models/movable_object.class.js
@@ -60,9 +60,15 @@ class MovableObject {
 
   moveRight() {
     this.x += this.speed;
+    this.otherDirection = false;
   }
 
   moveLeft() {
     this.x -= this.speed;
+    this.otherDirection = true;
+  }
+
+  jump() {
+    this.speedY = 30;
   }
 }
